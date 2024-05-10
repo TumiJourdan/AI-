a4f3dd11b9369a84f224c2ef764d6f807a383065
luca-vm <127728915+luca-vm@users.noreply.github.com>
2024-05-10 16:20:13 +0200

    chatified

--- part2_sub4.py
+++ part2_sub4.py
@@ -1,92 +1,44 @@
 import chess
 import reconchess.utilities as rutils
 
-# def generate_next_moves(board):
-#     next_moves = set() 
+def is_consistent_with_window(state_fen, window):
+    board = chess.Board(state_fen)
 
-#     pseudolegal_moves = board.pseudo_legal_moves
-#     for move in pseudolegal_moves:
-#         next_moves.add(move.uci())
+    observations = window.split(";")
+    for observation in observations:
+        square, piece = observation.split(":")
+        square_index = chess.parse_square(square)
+        observed_piece = piece.lower() if piece.islower() else piece.upper()
 
-#     next_moves.add(str(chess.Move.null()))
-    
-#     for move in rutils.without_opponent_pieces(board).generate_castling_moves():
-#         if not rutils.is_illegal_castle(board, move):
-#             next_moves.add(move.uci())
+        if board.piece_at(square_index):
+            current_piece = str(board.piece_at(square_index)).lower() if str(board.piece_at(square_index)).islower() else str(board.piece_at(square_index)).upper()
+            if current_piece != observed_piece:
+                return False
+        else:
+            if observed_piece != '?':
+                return False
 
-#     return sorted(next_moves)
+    return True
 
-def StatePredSense(states, window):
-    boards = []
-    
-    possible_states = []
-    
-    for i in states:
-        boards.append(chess.Board(i))
-
-    windowSquares = window.split(';')
-    
-    
-    for curr_board in boards:
-        for i, squares in enumerate(windowSquares):
-            square_piece = squares.split(':')
-            
-            square = chess.square(square_piece[0])
-            
-            if square_piece[1] == '?':
-                square_piece[1] = 'None'
-            
-            
-            if curr_board.piece_at(square) != square_piece[1]:
-                break
-            else:
-                possible_states.append(states[i])
-                
-    return possible_states
-            
-        
-            
-        
-
+def filter_states(states, window):
+    consistent_states = []
+    for state in states:
+        if is_consistent_with_window(state, window):
+            consistent_states.append(state)
+    return sorted(consistent_states)
 
 def main():
-    states = []
+
+    num_states = int(input())
+
+    potential_states = [input() for _ in range(num_states)]
     
-    n = int(input())
-    
-    for i in range(n):
-        states.append(input())
-        
     window = input()
     
-    pos_states = StatePredSense(states, window)
-    sorted_states = sorted(pos_states)
-    for state in sorted_states:
+    consistent_states = filter_states(potential_states, window)
+    
+    for state in consistent_states:
         print(state)
-    
-    
-    
-    # next_moves = generate_next_moves(board)
-    
-    
-    
-    # capture_moves = []
-    
-    # for move in next_moves:
-    #     if (move[2:4] == capture_move):
-    #         capture_moves.append(move)
-    
-    
-    # for move in capture_moves:
-    #     move = chess.Move.from_uci(move)
-    #     board_ins = chess.Board(fen_input)
-    #     board_ins.push(move)
-    #     next_states.add(board_ins.fen())
-
-    # sorted_states = sorted(next_states)
-    # for state in sorted_states:
-    #     print(state)
-        
 
 if __name__ == "__main__":
     main()
