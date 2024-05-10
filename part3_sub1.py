da5bf72bdb92b22aa8b2fd4d57439eb46c3e3027
luca-vm <127728915+luca-vm@users.noreply.github.com>
2024-05-10 16:47:29 +0200

    Update part3_sub1.py

--- part3_sub1.py
+++ part3_sub1.py
@@ -1,5 +1,34 @@
 import chess.engine
-engine = chess.engine.SimpleEngine.popen_uci('./stockfish', setpgrp=True)
+# engine = chess.engine.SimpleEngine.popen_uci('./stockfish', setpgrp=True)
+engine = chess.engine.SimpleEngine.popen_uci('/opt/stockfish/stockfish', setpgrp=True)
+
+def capture_opponent_king(board):
+    for move in board.legal_moves:
+        if board.is_capture(move) and board.piece_at(move.to_square) == chess.KING:
+            return move.uci()
+    return None
+
+def select_move_with_stockfish(board):
+    result = engine.play(board, chess.engine.Limit(time=0.5))
+    engine.quit()
+    return result.move.uci()
+
+def select_move(board):
+    capture_move = capture_opponent_king(board)
+    if capture_move:
+        return capture_move
+
+    return select_move_with_stockfish(board)
+
+def main():
+    fen_input = input() 
+    board = chess.Board(fen_input)  
+    move = select_move(board)  
+    print(move)
+    
+
+if __name__ == "__main__":
+    main()
 
 
 
@@ -8,5 +37,3 @@ engine = chess.engine.SimpleEngine.popen_uci('./stockfish', setpgrp=True)
 
 
 
-
-engine.quit()
